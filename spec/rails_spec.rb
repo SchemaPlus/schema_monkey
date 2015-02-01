e8dded086e4e36fab0362b331dc7b38553c6a02f
ronen barzel <ronen@barzel.org>
2015-02-01 00:52:42 +0000

    also test Rakefile insertion

--- spec/rails_spec.rb
+++ spec/rails_spec.rb
@@ -4,29 +4,39 @@ require 'schema_monkey/rails'
 
 describe SchemaMonkey::Rails do
 
-  Given {
+
+  before(:all) do
     # minimial setup for a rails app
     Kernel.const_set "Dummy", Class.new(Rails::Application) { config.eager_load = true }
     ENV['DATABASE_URL'] = "#{SchemaDev::Rspec.db_configuration[:adapter]}://localhost/dummy"
-  }
 
-  Given(:client) {
-    Module.new do
+    @client = Module.new do
       def self.insert
         @inserted = true
       end
       def self.inserted?
         @inserted
       end
+      def self.reset
+        @inserted = false
+      end
     end
-  }
 
-  Given { SchemaMonkey.register(client) }
+    SchemaMonkey.register(@client)
 
-  When {
+    Rake.application = Rake::Application.new
+    Rails.application.load_tasks
     Rails.application.initialize!
-  }
+  end
 
-  Then { expect(client).to be_inserted }
+  it "inserts client into app" do
+    expect(@client).to be_inserted
+  end
+
+  it "inserts client into rake" do
+    @client.reset
+    expect { Rake::Task["db:schema:dump"].invoke }.to raise_error(Errno::ENOENT)
+    expect(@client).to be_inserted
+  end
 
 end
