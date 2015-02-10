f5955e0003e01004f3471df591303e144b508294
ronen barzel <ronen@barzel.org>
2015-02-10 21:22:36 +0000

    test invalid module

--- spec/active_record_spec.rb
+++ spec/active_record_spec.rb
@@ -4,7 +4,7 @@ $client_count = 0
 
 describe SchemaMonkey::ActiveRecord do
 
-  before(:each) {
+  When(:result) {
     SchemaMonkey.register(client)
     SchemaMonkey.insert
     ActiveRecord::Base.establish_connection :schema_dev
@@ -16,23 +16,23 @@ describe SchemaMonkey::ActiveRecord do
 
     [:prepend, :include].each do |mode|
 
-      context "#{mode} general module"+suffix do
-        let(:client) { make_client('ConnectionAdapters::SchemaStatements', mode: mode, class_methods: class_methods) }
-        it { expect_inserted(ActiveRecord::ConnectionAdapters::SchemaStatements, client.module, mode, class_methods) }
+      context "with #{mode} general module"+suffix do
+        Given(:client) { make_client('ConnectionAdapters::SchemaStatements', mode: mode, class_methods: class_methods) }
+        Then { expect_inserted(ActiveRecord::ConnectionAdapters::SchemaStatements, client.module, mode, class_methods) }
       end
 
       SchemaMonkey::DBMS.each do |dbm|
 
-        context "#{mode} #{dbm} module"+suffix do
-          let(:client) { make_client("ConnectionAdapters::#{dbm}::SchemaStatements", mode: mode, class_methods: class_methods) }
+        context "with #{mode} #{dbm} module"+suffix do
+          Given(:client) { make_client("ConnectionAdapters::#{dbm}::SchemaStatements", mode: mode, class_methods: class_methods) }
 
           SchemaMonkey::DBMS.each do |loaded_dbm|
 
-            context "when #{loaded_dbm}", loaded_dbm.to_s.downcase.to_sym => :only do
+            context "if using #{loaded_dbm}", loaded_dbm.to_s.downcase.to_sym => :only do
               if dbm == loaded_dbm
-                it { expect_inserted(ActiveRecord::ConnectionAdapters::SchemaStatements, client.module, mode, class_methods) }
+                Then { expect_inserted(ActiveRecord::ConnectionAdapters::SchemaStatements, client.module, mode, class_methods) }
               else
-                it { expect_not_inserted(ActiveRecord::ConnectionAdapters::SchemaStatements, client.module, class_methods) }
+                Then { expect_not_inserted(ActiveRecord::ConnectionAdapters::SchemaStatements, client.module, class_methods) }
               end
             end
           end
@@ -40,6 +40,11 @@ describe SchemaMonkey::ActiveRecord do
       end
 
     end
+  end
+
+  context "with invalid module" do
+    Given(:client) { make_client('ConnectionAdapters::NoSuchModule') }
+    Then { expect(result).to have_failed(SchemaMonkey::InsertionError) }
   end
 
   private
@@ -59,7 +64,7 @@ describe SchemaMonkey::ActiveRecord do
     expect(base.ancestors).not_to include mod
   end
 
-  def make_client(path, mode:, class_methods: nil)
+  def make_client(path, mode: nil, class_methods: nil)
     name = "TestActiveRecord#{$client_count}"
     $client_count += 1
     Object.send :remove_const, name if Object.const_defined? name
